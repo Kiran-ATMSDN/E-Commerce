a9368ef1fe2498aafda3ca96f4c5fd115c018c83
Kiran-ATMSDN <dkiran2949@gmail.com>
2023-12-16 14:04:23 +0530

    Changes in db.js to check db connection

--- Backend/db.js
+++ Backend/db.js
@@ -2,10 +2,29 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "P@ssw0rd",
+  password: "postgres",
   host: "localhost",
   port: 5432,
-  database: "e_commerce", 
+  database: "e_commerce",
+});
+
+// Check the database connection
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the database');
+    // Perform additional queries or operations here if needed
+
+    // Release the client back to the pool
+    done();
+  }
+});
+
+// Optionally, you can listen for pool errors
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
